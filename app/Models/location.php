6b4da999c726b2e253ab2f6665e222964efbef7c
Eric Yu <fanfanfan9453@gmail.com>
2021-01-30 07:27:03 +0000

    修改location model

--- app/Models/location.php
+++ app/Models/location.php
@@ -10,9 +10,13 @@ class location extends Model
     use HasFactory;
     protected $table = 'location';
     protected $primaryKey = 'id';
-    protected $fillable = ['name', 'counties_id'];
+    protected $fillable = ['name', 'countie_id'];
     public function location()
     {
         return $this->hasOne('App\Models\countie');
+    }
+    public function countie()
+    {
+        return $this->belongsTo('App\Models\countie');
     }
 }
